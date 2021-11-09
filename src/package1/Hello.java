f61a88eeb88914d28799ce139f0ddf150fa81de2
EmsCode82 <emsgithub@gmail.com>
2021-11-09 15:21:56 -0600

    .

---
+++ src/package1/Hello.java
@@ -0,0 +1,39 @@
+package package1;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		/*System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+		System.out.println(5);
+		System.out.println("6\n7\n8\n9\n10");
+		*/
+		
+		/*	int i = 10;
+		for(;;) {
+			
+			System.out.print("Value of i:");
+			System.out.println(i);
+			if(i<=1) {
+				break;
+			}
+			i--;
+			*/
+		
+		for(int i = 1000; i>=1 ; i--) {
+			
+			System.out.print("Value of i:");
+			System.out.print("\n");
+			System.out.println(i);
+			
+		//for(;;) {
+		//	System.out.println("Testing text");  INFINITE LOOP! DO NOT RUN!!!
+		
+		}
+		
+	}
+
+}
